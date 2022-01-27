b16e49dd790bf3cf7f110b8b62545c9a75c995d3
Yutaka <y.tajika529@gmail.com>
2022-01-27 13:52:49 +0900

    add receiving user input to generate VIPERGenfile

--- Sources/VIPERGen/InitCommand.swift
+++ Sources/VIPERGen/InitCommand.swift
@@ -1,15 +1,24 @@
 import ArgumentParser
 import Foundation
+import VIPERGenKit
 
 extension Executor {
     struct InitCommand: ParsableCommand {
         static var configuration = CommandConfiguration(commandName: "init", abstract: "")
 
-        @Option(help: "path to VIPERGenfile.", completion: .directory)
+        @Option(help: "direcotry path to VIPERGenfile.", completion: .directory)
         var vipergenfilePath: String?
 
         mutating func run() throws {
             print("init command.")
+            // TODO: VIPERGenfile の有無チェック
+            print(FileManager.default.viperGenfileExists(atDirectory: vipergenfilePath ?? "."))
+
+            print("Enter your Company Name: ", terminator: "")
+            guard let companyName = readLine() else {
+                return
+            }
+            print(companyName)
         }
     }
 }
