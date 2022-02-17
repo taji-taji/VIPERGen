5e5c915ab0caf97eb66c496a8cb70c1e93cc62f0
Yutaka <y.tajika529@gmail.com>
2022-02-17 14:10:39 +0900

    fixed: ファイル操作周りの修正

--- Sources/VIPERGenKit/Gyb.swift
+++ Sources/VIPERGenKit/Gyb.swift
@@ -1,23 +1,28 @@
 import Foundation
 
 struct Gyb {
-    private let path: String
+    private let pathURL: URL
     private let fileManager: FileManager
     
     init(path: String, fileManager: FileManager = .default) throws {
-        self.path = path
+        let nsStringPath = NSString(string: path)
+        self.pathURL = URL(fileURLWithPath: nsStringPath.expandingTildeInPath, relativeTo: URL(fileURLWithPath: #file))
         self.fileManager = fileManager
+        
+        print(fileManager.fileExists(atPath: pathURL.path))
 
-        if !fileManager.fileExists(atPath: path) {
+        if !fileManager.fileExists(atPath: pathURL.path) {
             throw GybError.gybNotFound
         }
     }
 
     func generate(templatePath: String, outputPath: String, arguments: [String : String]) throws {
-        guard fileManager.fileExists(atPath: templatePath) else {
+        let nsStringPath = NSString(string: templatePath)
+        let url = URL(fileURLWithPath: nsStringPath.expandingTildeInPath, relativeTo: URL(fileURLWithPath: #file))
+        guard fileManager.fileExists(atPath: url.path) else {
             throw GybError.templateNotFound
         }
-        let argumentsList: [String] = [path, templatePath, "-o", outputPath, "--line-directive="] + arguments.map { "-D" + $0.key + "=" + $0.value }
+        let argumentsList: [String] = [pathURL.path, templatePath, "-o", outputPath, "--line-directive="] + arguments.map { "-D" + $0.key + "=" + $0.value }
         shell(argumentsList)
     }
 }
